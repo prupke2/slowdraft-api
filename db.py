6e0080da3f6129801a3876d5c40b1d6cb638b933
Paul Rupke <paul.rupke@gmail.com>
2022-05-22 15:58:48 -0400

    lint db

--- db.py
+++ db.py
@@ -3,15 +3,16 @@ import pymysql.cursors
 import config
 import app
 
+
 class DB(object):
 
-	def __init__(self):
-		self.connection = pymysql.connect(
-			host=config.host,
-			user=config.user,
-			password=config.password,
-			database=config.db,
-			charset='utf8mb4', 
-			cursorclass=pymysql.cursors.DictCursor
-		)
-		self.cur = self.connection.cursor()
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host=config.host,
+            user=config.user,
+            password=config.password,
+            database=config.db,
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        self.cur = self.connection.cursor()
