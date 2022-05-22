2a47f7e2d8f686843840f7c4b07f3e8d9d224173
Paul Rupke <paul.rupke@gmail.com>
2022-05-22 15:59:16 -0400

    lint util

--- util.py
+++ util.py
@@ -1,28 +1,32 @@
 import db
 import datetime
 
-def return_true():
-  return { "success": True }
 
-def return_error(message, status = 400):
-  return {
-      "success": False,
-      "error": message,
-      "status": status
+def return_true():
+    return {"success": True}
+
+
+def return_error(message, status=400):
+    return {
+        "success": False,
+        "error": message,
+        "status": status
     }
 
+
 def update(table, draft_id):
-  database = db.DB()
-  query = f""" UPDATE updates 
+    database = db.DB()
+    query = f""" UPDATE updates
 		SET {table} = %s
 		WHERE draft_id = %s
 	"""
-  database.cur.execute(query, (datetime.datetime.utcnow(), draft_id))
-  database.connection.commit()
-  return
+    database.cur.execute(query, (datetime.datetime.utcnow(), draft_id))
+    database.connection.commit()
+    return
+
 
 def get_last_row_inserted(table, column):
-  database = db.DB()
-  database.cur.execute(f"SELECT MAX({column}) FROM {table};")
-  max = database.cur.fetchone()
-  return max[f'MAX({column})']
+    database = db.DB()
+    database.cur.execute(f"SELECT MAX({column}) FROM {table};")
+    max = database.cur.fetchone()
+    return max[f'MAX({column})']
