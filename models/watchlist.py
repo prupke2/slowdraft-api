24c4a9ba0bf54b00ca7549cff09167dcca06fe37
Paul Rupke <paul.rupke@gmail.com>
2023-09-23 23:18:49 -0400

    update watchlist functions

--- models/watchlist.py
+++ models/watchlist.py
@@ -5,45 +5,36 @@ from db import *
 class WatchlistForm(BaseModel):
 		player_id: int
 
-def get_watchlist():
+def get_watchlist_ids(yahoo_league_id, team_key):
 	database = db.DB()
-	sql = f"SELECT DISTINCT w.player_id, y.* FROM watchlist w INNER JOIN {YAHOO_PLAYER_DB} y ON y.player_id = w.player_id \
-			WHERE yahoo_league_id = %s AND user_id = %s"
-	database.cur.execute(sql, (session['yahoo_league_id'], session['user_id']))
+	sql = f"""
+		SELECT w.player_id 
+		FROM watchlist w 
+		INNER JOIN {YAHOO_PLAYER_DB} y 
+			ON y.player_id = w.player_id 
+		WHERE yahoo_league_id = %s 
+		AND team_key = %s
+	"""
+	database.cur.execute(sql, (yahoo_league_id, team_key))
 	players = database.cur.fetchall()
-	skaters = []
-	goalies = []
-	for player in players:
-		if player['position'] == "G":
-			goalies.append(player)
-		else:
-			skaters.append(player)
-			
-	return skaters, goalies
 
-def get_watchlist_ids():
-	database = db.DB()
-	sql = f"SELECT w.player_id FROM watchlist w INNER JOIN {YAHOO_PLAYER_DB} y ON y.player_id = w.player_id WHERE yahoo_league_id = %s AND user_id = %s"
-	database.cur.execute(sql, (session['yahoo_league_id'], session['user_id']))
-	players = database.cur.fetchall()
-	print("\n\nPlayers: " + str(players))
 	watchlist = []
 	for player in players:
 		watchlist.append(player['player_id'])
-		
 	return watchlist
 
 def add_player_to_watchlist(yahoo_league_id, team_key, player_id):
 	database = db.DB()
-	sql = "INSERT INTO watchlist(yahoo_league_id, user_id, player_id) VALUES(%s, %s, %s)"
+	sql = "INSERT INTO watchlist(yahoo_league_id, team_key, player_id) VALUES(%s, %s, %s)"
 	print(sql)
-	database.cur.execute(sql, (session['yahoo_league_id'], session['user_id'], id))
+	database.cur.execute(sql, (yahoo_league_id, team_key, player_id))
 	database.connection.commit()
-	# return True
+	return util.return_true()
 
 def remove_player_from_watchlist(yahoo_league_id, team_key, player_id):
 	database = db.DB()
-	sql = "DELETE FROM watchlist WHERE yahoo_league_id = %s AND user_id = %s AND player_id = %s"
+	sql = "DELETE FROM watchlist WHERE yahoo_league_id = %s AND team_key = %s AND player_id = %s"
 	print(sql)
-	database.cur.execute(sql, (session['yahoo_league_id'], session['user_id'], id))
+	database.cur.execute(sql, (yahoo_league_id, team_key, player_id))
 	database.connection.commit()
+	return util.return_true()
