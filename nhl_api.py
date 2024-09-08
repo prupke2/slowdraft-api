94b032b919f8083ae5f666b969d1673809f2b415
Paul Rupke <paul.rupke@gmail.com>
2024-09-08 00:45:21 -0400

    add new functions for new nhl api

--- nhl_api.py
+++ nhl_api.py
@@ -2,52 +2,24 @@ from app import *
 from db import *
 import util
 
-def getNHLId(name):
-    NHL_PLAYER_SEARCH_URL = "https://suggest.svc.nhl.com/svc/suggest/v1/min_all/" + name + "/1"
-    response = requests.get(NHL_PLAYER_SEARCH_URL)
+def get_nhl_id_from_db(player_id):
+    # TODO: build a replacement for getting nhl_id from nhl's api
+    database = db.DB()
+    sql = "SELECT nhl_id FROM yahoo_db_23_24 WHERE player_id = %s"
+    database.cur.execute(sql, [player_id])
+    player = database.cur.fetchone()
+    if player:
+        return player[0]
+    return None
+
+def get_career_gp(nhl_id):
+    PLAYER_DATA_ENDPOINT = f"https://api-web.nhle.com/v1/player/{nhl_id}/landing"
+    response = requests.get(PLAYER_DATA_ENDPOINT)
+
     if util.valid_2XX_response(response):
-        result = response.json()
-        if result['suggestions'] != []:
-            return result['suggestions'][0][2:9]
-        else:
-            return 0
-    else:
-        print("There was an error hitting NHL API: \n\n" +
-              str(response.status_code) + " " + str(response.json))
-        return 0
-
-
-def getCareerGamesPlayed(id):
-    NHL_PLAYER_INFO_URL = f"https://statsapi.web.nhl.com/api/v1/people/{str(id)}?expand=person.stats&stats=careerRegularSeason"
-    response = requests.get(NHL_PLAYER_INFO_URL)
-    if util.valid_2XX_response(response):
-        result = response.json()
-        if result['people'][0]['stats'][0]['splits'] == []:
-            return 0
-        else:
-            return result['people'][0]['stats'][0]['splits'][0]['stat']['games']
-
-
-def getNhlDraftRound(year, round):
-    NHL_DRAFT_YEAR_URL = f"https://statsapi.web.nhl.com/api/v1/draft/{year}"
-    response = requests.get(NHL_DRAFT_YEAR_URL)
-    if util.valid_2XX_response(response):
-        result = response.json()
-        return result['drafts'][0]['rounds'][round - 1]['picks']
-    else:
-        print(
-            f"Error getting draft for year {year}, round {round}. Response: {response}")
-        return null
-
-
-def getNhlProspect(prospect_url):
-    NHL_PROSPECT_URL = f"https://statsapi.web.nhl.com{prospect_url}"
-    print(f"NHL_PROSPECT_URL: {NHL_PROSPECT_URL}")
-    response = requests.get(NHL_PROSPECT_URL)
-    if util.valid_2XX_response(response):
-        result = response.json()
-        return result['prospects'][0]
-    else:
-        print(
-            f"Error getting prospect with url: {prospect_url}. Response: {response}")
-        return null
+        result = response.json()        
+        if "careerTotals" in result:
+            if "regularSeason" in result["careerTotals"]:
+                if "gamesPlayed" in result["careerTotals"]["regularSeason"]:
+                    return result["careerTotals"]["regularSeason"]["gamesPlayed"]
+    return None
